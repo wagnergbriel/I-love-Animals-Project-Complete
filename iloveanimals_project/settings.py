74ebfc1c42231fb11f42a09f7c820f0eae55c8e5
wagnergbriel <wagnergabriel220596@gmail.com>
2021-12-02 19:01:56 -0300

    refactor: adicionar variavel de ambiente no arquivo settings

--- iloveanimals_project/settings.py
+++ iloveanimals_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kg$4h4+*w&5xsm50sl^rmi#y1o)w9fm*j2%w82tn1sa44ozegz'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
