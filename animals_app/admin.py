16131d137ff5bdb8b5cd83095950e4ba1aec7fe8
wagnergbriel <wagnergabriel220596@gmail.com>
2021-12-07 20:18:59 -0300

    feat: Correcao animals_app/models.py e admin.py

--- animals_app/admin.py
+++ animals_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Shelter, Animals
 
-# Register your models here.
+admin.site.register(Shelter)
+admin.site.register(Animals)
--- animals_app/models.py
+++ animals_app/models.py
@@ -3,40 +3,28 @@ from pyUFbr.baseuf import ufbr
 import uuid
 
 class Shelter(models.Model):
-    """ class which struct fields of type of personal identifier"""
+    """class which struct fields of type of personal identifier"""
     class TypePersonalIdentifier(models.TextChoices):
-        cpf = 'CPF'
-        cnpj = 'CNPJ'
+        CPF = 'CPF'
+        CNPJ = 'CNPJ'
     
-    """ class which return organizacion struct of identifier"""
-    class PersonalIdentifierField(models.Field):
-        def __init__(self, connection, *args, **kwargs):
-            self.connection = connection
-            super().__init__(*args, **kwargs)
-        
-        def db_type(self, connection):
-            if self.connection == 'CPF':
-                return 'char(9)'
-            if self.connection == 'CNPJ':
-                return 'char(12)'
-
+    """Fields of class Shelter"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     animals = models.ForeignKey('Animals' ,on_delete=models.PROTECT)
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=14)
     email = models.EmailField()
     password = models.CharField(max_length=12)
-    type_personal_identifier = models.CharField(max_length=50, choices=TypePersonalIdentifier.choices)
-    personal_identifier = PersonalIdentifierField(type_personal_identifier)
+    type_personal_identifier = models.CharField(max_length=10, choices=TypePersonalIdentifier.choices, default="CNPJ")
+    personal_identifier = models.CharField(max_length=14)
     address = models.CharField(max_length=200)
     number_address = models.IntegerField()
     cep = models.CharField(max_length=8)
-    state = models.CharField(max_length=50, choices=ufbr.list_uf)
-    city = models.CharField(max_length=50, choices=ufbr.list_cidades(state))
+    state = models.CharField(max_length=10, choices=[(state, state) for state in ufbr.list_uf])
+    city = models.CharField(max_length=200)
     district = models.CharField(max_length=200)
     complement = models.CharField(max_length=500)
-    
-
+   
     def __str__(self):
         return self.name
 
@@ -46,6 +34,7 @@ class Animals(models.Model):
         CAT = 'cat'
         DOG = 'dog'
     
+    """Fields of class Animal"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type_animal = models.CharField(max_length=50, choices=TypeAnimals.choices)
     name = models.CharField(max_length=200)
