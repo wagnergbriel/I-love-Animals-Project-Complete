d647e354811f711de7c43649ad392d51866cf56f
wagnergbriel <wagnergabriel220596@gmail.com>
2021-12-30 20:56:30 -0300

    feat: incluir rota animals_app e atualizar config

--- iloveanimals/settings.py
+++ iloveanimals/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILE_DIRS = [
+    os.path.join(BASE_DIR, 'animals_app/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
