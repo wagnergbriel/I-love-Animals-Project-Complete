96c982b609fb6d0800f05af9f7013e8f50eb0c2f
wagnergbriel <wagnergabriel220596@gmail.com>
2022-01-17 16:43:18 -0300

    feat: adicionar url login

--- animals_app/urls.py
+++ animals_app/urls.py
@@ -3,5 +3,6 @@ from animals_app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("adocao/", views.adocao, name="adocao")
+    path("adocao/", views.adocao, name="adocao"),
+    path("login/", views.login, name="login")
 ]
